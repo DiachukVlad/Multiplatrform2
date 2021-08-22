f25566e6f50327ea4cff18a8896842b824488113
vlad <vladiachuk@gmail.com>
2021-08-22 11:57:47 +0200

    simple kotlin js

--- browser/src/main/kotlin/Simple.kt
+++ browser/src/main/kotlin/Simple.kt
@@ -1,9 +1,17 @@
 import kotlinx.browser.document
+import kotlinx.browser.window
 import kotlinx.dom.appendElement
+import kotlinx.html.div
+import kotlinx.html.dom.append
+import kotlinx.html.js.div
+import kotlinx.html.js.h1
+import org.w3c.dom.Node
 
 fun main() {
-    console.log("Hello, ${greet()}")
-//    document.getElementById("root").app
+    document.getElementById("root")?.append {
+        h1 {
+            +"Hello Inna Kostiuk!"
+        }
+    }
+    println(document)
 }
-
-fun greet() = "world"
